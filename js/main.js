110c3e604fd3b259766a6ec731673737b45c76c0
AgustinElizalde <agustinelizalde93@gmail.com>
2023-11-18 16:04:51 -0300

    Se agrego una funcion nueva

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function menuAnimacion(){
     document.getElementById("hamburger").classList.toggle("menu-activo");
     document.getElementById("slide").classList.toggle("slide-activo");
 }
+
+function redireccionar(url) {
+    window.location.href = url;
+}
